c32de3de18037e296d5d14777a70f165d802b6a7
root <root@DESKTOP-P110KQ5.localdomain>
2024-05-27 23:19:57 +0500

    Fix: Banquet Booking validation

--- banquet_management_system/banquet_management_system/doctype/banquet_booking/banquet_booking.py
+++ banquet_management_system/banquet_management_system/doctype/banquet_booking/banquet_booking.py
@@ -11,7 +11,9 @@ class BanquetBooking(Document):
         self.posting_datetime = now()
         if not self.selected_menu:
             frappe.throw(_('Please select menu items to proceed to save'))
-            
+        
+        self.validate_dates()
+        self.validate_banquet()            
         self.calculate_totals()
         
         if self.grand_total <= 0:
@@ -27,3 +29,27 @@ class BanquetBooking(Document):
         self.total_per_person = total_per_person
         self.grand_total = self.total_per_person * cint(self.expected_persons)
         self.total_in_words = money_in_words(self.grand_total)
+
+    def validate_banquet(self):
+        query = """
+            SELECT COUNT(*) 
+            FROM `tabBanquet Booking` 
+            WHERE docstatus = 1
+            AND visitor = %(visitor)s
+            AND banquet = %(banquet)s 
+            AND event_from >= %(event_from)s 
+            AND event_to <= %(event_to)s;
+        """
+        params = {
+            'visitor': self.visitor,
+            'banquet': self.banquet,
+            'event_from': self.event_from,
+            'event_to': self.event_to
+        }
+        banquet_count = frappe.db.sql(query, params)[0][0]
+        if banquet_count > 0:
+            frappe.throw(_(f'Banquet {self.banquet} is already booked between provided dates'))
+
+    def validate_dates(self):
+        if self.event_from > self.event_to:
+            frappe.throw(_('Event From date should not be less than Event To.'))
