ea358919ead8037865a98df7c4420ad95c9ce4c2
aliraxa29 <soundsofnation29@gmail.com>
2024-05-27 20:33:43 +0500

    Updated Banquet booking with calculations and selected menu with field value fetching

--- banquet_management_system/banquet_management_system/doctype/banquet_booking/banquet_booking.js
+++ banquet_management_system/banquet_management_system/doctype/banquet_booking/banquet_booking.js
@@ -6,3 +6,29 @@
 
 // 	},
 // });
+
+frappe.ui.form.on("Selected Menu", {
+    qty: function(frm, cdt, cdn) {
+        calculateTotals(frm, cdt, cdn);
+        setTotalValue(frm);
+    },
+    price: function(frm, cdt, cdn) {
+        calculateTotals(frm, cdt, cdn);
+        setTotalValue(frm);
+    }
+});
+
+function calculateTotals(frm, cdt, cdn) {
+    let row = locals[cdt][cdn];
+    let amount = row.qty * row.price;
+    frappe.model.set_value(cdt, cdn, 'amount', amount);
+}
+
+function setTotalValue(frm) {
+    var total_amount = 0;
+    $.each(frm.doc.selected_menu || [], function(i, row) {
+        total_amount += row.amount || 0;
+    });
+    frm.set_value('grand_total', total_amount);
+    frm.set_value('total_in_words', frappe.utils.money_in_words(total_amount))
+}
--- banquet_management_system/banquet_management_system/doctype/banquet_booking/banquet_booking.py
+++ banquet_management_system/banquet_management_system/doctype/banquet_booking/banquet_booking.py
@@ -1,9 +1,27 @@
 # Copyright (c) 2024, Ali Raza and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe import _
+from frappe.utils import money_in_words, cint
 
 class BanquetBooking(Document):
-	pass
+    def validate(self):
+        if not self.selected_menu:
+            frappe.throw(_('Please select menu items to proceed to save'))
+            
+        self.calculate_totals()
+        
+        if self.grand_total <= 0:
+            frappe.throw(_('Grand Total should be greater than zero'))
+        
+    
+    def calculate_totals(self):
+        grand_total = 0
+        for item in self.selected_menu:
+            item.amount = item.qty * item.price
+            grand_total += item.amount
+            
+        self.grand_total = grand_total
+        self.total_in_words = money_in_words(self.grand_total)
