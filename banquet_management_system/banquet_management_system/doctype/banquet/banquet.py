b7830977129a02ae6eee2634973eac54010d1f5f
Ali Raza <soundsofnation29@gmail.com>
2024-05-27 23:51:32 +0500

    Fix: validated and insering item using banquet

--- banquet_management_system/banquet_management_system/doctype/banquet/banquet.py
+++ banquet_management_system/banquet_management_system/doctype/banquet/banquet.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2024, Ali Raza and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
 
 class Banquet(Document):
-	pass
+    
+    def validate(self):
+        if frappe.db.count('Item', {'item_code': self.banquet_name}) > 0:
+            frappe.throw(_('Item with this banquet is already exists.'))
+            
+    
+    def after_insert(self):
+        doc = frappe.get_doc({
+			'doctype': 'Item',
+			'item_code': self.banquet_name,
+			'item_name': self.banquet_name,
+			'item_group': 'Services'
+		})
+        doc.insert(ignore_permissions=True)
